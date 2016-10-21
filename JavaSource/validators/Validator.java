94d2607dcea1eb89ecae372d33c15d46b2ebecfa
loganborean <loganborean@gmail.com>
2016-10-20 23:24:07 -0700

    fix unique sheet bug

--- JavaSource/validators/Validator.java
+++ JavaSource/validators/Validator.java
@@ -2,23 +2,44 @@ package validators;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.html.HtmlDataTable;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.component.datatable.DataTable;
+
 import annotations.NoDB;
 import ca.bcit.infosys.employee.EmployeeList;
+import ca.bcit.infosys.timesheet.TimesheetRow;
+import services.TimesheetService;
 
 @Named("validator")
 @SessionScoped
 public class Validator implements Serializable {
 	
 	@Inject @NoDB private EmployeeList employeeList;
+
+	@Inject TimesheetService sheetService;
+	
+	private HtmlDataTable ddataTable;
+	
+	public HtmlDataTable getDdataTable() {
+		return this.ddataTable;
+	}
+	
+	public void setDdataTable(HtmlDataTable table) {
+		ddataTable = table;
+	}
+
 
 	public void validatePassword(FacesContext context, UIComponent componentToValidate, 
 												       Object value) throws ValidatorException {
@@ -114,7 +135,7 @@ public class Validator implements Serializable {
 	}
 
 	public void validateHours(FacesContext context, UIComponent componentToValidate, 
-												       Object value) throws ValidatorException {
+												    Object value) throws ValidatorException {
 		if (value == null) return;
 		
 		BigDecimal hours = (BigDecimal) value;
@@ -124,6 +145,49 @@ public class Validator implements Serializable {
 				new FacesMessage("Hours must be between 0 and 24"));
 		}
 
+	}
+
+	public void validateWP(FacesContext context, UIComponent componentToValidate, 
+												    Object value) throws ValidatorException {
+
+		String wp = (String) value;
+
+		if (wp == null || wp.length() == 0) {
+			throw new ValidatorException(
+				new FacesMessage("You must enter a Work Package"));
+		}
+
+		if (wp.length() > 10) {
+			throw new ValidatorException(
+				new FacesMessage("name must be less than 10 characters"));
+		}
+
+		for(int i = 0; i < wp.length(); i++) {
+			if (!Character.isLetter(wp.charAt(i)) && 
+				!Character.isDigit(wp.charAt(i))) { 
+				throw new ValidatorException(
+					new FacesMessage("Only letters and digits in WP"));
+			}
+		}
+
+		List<TimesheetRow> rows = sheetService.getCurrentSheet().getDetails();
+		UIInput proj = (UIInput) componentToValidate.findComponent("projID");
+		
+		int projectId = ((Integer) proj.getLocalValue()).intValue();
+		
+		TimesheetRow currentlyEditingRow = (TimesheetRow) componentToValidate.getAttributes().get("currentRow"); 
+
+		for(TimesheetRow row : rows) {
+			Integer thisRowsID = row.getProjectID();
+
+			if (thisRowsID == null) continue;
+
+			if (row != currentlyEditingRow && row.getProjectID().equals(projectId) && row.getWorkPackage().equals(wp)) {
+				throw new ValidatorException(
+					new FacesMessage("Work package must be unique for each project"));
+				
+			}
+		}
 	}
 
 
